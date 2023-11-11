18fc6e54a25e71d5370c8ffad695792434fbc468
Martin Nordholts <enselic@gmail.com>
2023-11-11 16:50:46 +0100

    wip  2023-11-11T16:50:46+01:00

--- src/bin/plot-opened-and-closed/main.rs
+++ src/bin/plot-opened-and-closed/main.rs
@@ -6,7 +6,7 @@ type DateTime = chrono::DateTime<chrono::Utc>;
 #[derive(graphql_client::GraphQLQuery)]
 #[graphql(
     schema_path = "schemas/github_schema.graphql",
-    query_path = "src/bin/plot-opened-and-closed/opened-and-closed.graphql",
+    query_path = "src/bin/plot-opened-and-closed/OpenedAndClosedIssues.graphql",
     variables_derives = "Clone, Debug",
     response_derives = "Clone, Debug"
 )]
@@ -42,7 +42,6 @@ async fn main() -> anyhow::Result<()> {
     };
 
     let pages_left = args.pages;
-    let mut page = 1;
     loop {
         if pages_left == 0 {
             break;
@@ -58,7 +57,6 @@ async fn main() -> anyhow::Result<()> {
             )
             .await?;
 
-        println!("Page {page}:");
         let issues = &response
             .data
             .as_ref()
@@ -69,13 +67,20 @@ async fn main() -> anyhow::Result<()> {
             .issues;
         println!("{issues:?}");
 
+        if issues.nodes.is_empty() {
+            break;
+        }
+
+        if issues.nodes.is_empty() {
+            break;
+        }
+
         if !update_page_info(&mut variables, issues) {
             break;
         }
 
         match response {
-            Ok(response) => {
-            }
+            Ok(response) => {}
             Err(error) => {
                 println!("{error:#?}");
                 break;
@@ -85,34 +90,6 @@ async fn main() -> anyhow::Result<()> {
         page += 1;
     }
 
-    github
-        .for_issues_with_timeline(
-            variables,
-            args.pages,
-            |issue| {
-                let (label_age, comment_age) = get_ages(issue).unwrap();
-                let label_age_months = label_age.to_months();
-                let comment_age_months = comment_age.to_months();
-
-                let old_enough = label_age_months > args.label_months_considered_old
-                    && comment_age_months > args.last_comment_months_considered_old;
-
-                let labeled_triaged = issue
-                    .labels
-                    .nodes
-                    .iter()
-                    .any(|label| label.name.to_lowercase().contains("triaged"));
-
-                if old_enough && !labeled_triaged {
-                    println!(
-                        "{} E-needs-mcve {} months old, last comment {} months ago",
-                        issue.url, label_age_months, comment_age_months
-                    );
-                }
-            },
-            || {},
-        )
-        .await;
 
     Ok(())
 }
